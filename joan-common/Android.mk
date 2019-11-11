3e18e10783c2943a562a98a0d066fc5d8fddbc35
Nicholas Wereszczak <nwereszczak@users.noreply.github.com>
2019-11-11 15:49:13 -0500

    Add us998

--- joan-common/Android.mk
+++ joan-common/Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter h930 h932,$(TARGET_DEVICE)),)
+ifneq ($(filter h930 h932,us998,$(TARGET_DEVICE)),)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libantradio
